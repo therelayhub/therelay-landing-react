7a475b68500c0967c2fc0a8f637918153555e223
deadvait28 <advaitnaik28@gmail.comm>
2025-04-05 11:39:05 +0100

    Updated to add web specific images and copy

---
+++ src/hooks/useInView.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+const useInView = (ref, options = {}) => {
+  const [isInView, setIsInView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsInView(entry.isIntersecting);
+    }, options);
+
+    const currentRef = ref.current;
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [ref, options]);
+
+  return isInView;
+};
+
+export default useInView; 
